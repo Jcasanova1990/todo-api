67218e48aacdbd1ebac606d607d3be307aec8996
Jcasanova1990 <jeremykcasanova@gmail.com>
2024-01-20 14:44:48 -0500

    "updated try and catch codes"

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,57 +1,51 @@
 const todo = require('../models/todo');
 const jwt = require('jsonwebtoken');
 
-
 // jwt generate authentication token
 exports.generateAuthToken = async function() {
   try {
     const token = jwt.sign({ _id: this._id }, 'secret');
     return token;
   } catch (error) {
-    // Handle the error appropriately, e.g., log it or return an error response
     console.error('Error generating auth token:', error);
     throw new Error('Internal Server Error');
   }
 };
 
 // GET /todos: Retrieve all todo items
-exports.getAllTodos = async (req, res, next) => {
+exports.getAllTodos = async (req, res) => {
   try {
     const todos = await todo.find();
     res.status(200).json(todos);
   } catch (error) {
-    res.status(500).send('Internal Server Error');
+    res.status(400).send('Bad Request');
   }
 };
 
 // POST /todos: Create a new todo item
-exports.createTodo = async (req, res, next) => {
+exports.createTodo = async (req, res) => {
   try {
     const { title, description, completed} = req.body;
     const newTodo = new todo({ title, description, completed });
     await newTodo.save();
     res.status(200).json(newTodo);
   } catch (error) {
-    res.status(500).send('Internal Server Error');
+    res.status(400).send('Bad Request');
   }
 };
 
 // GET /todos/:id: Retrieve a specific todo item identified by its ID
-exports.getTodoById = async (req, res, next) => {
+exports.getTodoById = async (req, res) => {
   try {
     const todoItem = await todo.findById(req.params.id);
-    if (!todoItem) {
-      res.status(404).send('Todo not found');
-    } else {
-      res.status(200).json(todoItem);
-    }
+    res.status(200).json(todoItem); // Move this line inside the try block
   } catch (error) {
-    res.status(500).send('Internal Server Error');
+    res.status(400).send('Bad Request');
   }
 };
 
 // PUT /todos/:id: Update a specific todo item identified by its ID
-exports.updateTodoById = async (req, res, next) => {
+exports.updateTodoById = async (req, res) => {
   try {
     const { title, description } = req.body;
     const updatedTodo = await todo.findByIdAndUpdate(
@@ -59,26 +53,18 @@ exports.updateTodoById = async (req, res, next) => {
       { title, description },
       { new: true, runValidators: true }
     );
-    if (!updatedTodo) {
-      res.status(404).send('Todo not found');
-    } else {
-      res.status(200).json(updatedTodo);
-    }
+    res.status(200).json(updatedTodo);
   } catch (error) {
-    res.status(500).send('Internal Server Error');
+    res.status(400).send('Bad Request');
   }
 };
 
 // DELETE /todos/:id: Delete a specific todo item identified by its ID
-exports.destroyTodoById = async (req, res, next) => {
+exports.destroyTodoById = async (req, res) => {
   try {
     const deletedTodo = await todo.findByIdAndDelete(req.params.id);
-    if (!deletedTodo) {
-      res.status(404).send('Todo not found');
-    } else {
-      res.status(200).json(deletedTodo);
-    }
+    res.status(200).json(deletedTodo);
   } catch (error) {
-    res.status(500).send('Internal Server Error');
+    res.status(400).send('Bad Request');
   }
 };
